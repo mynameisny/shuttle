f9e3ad8711d2ef5bd2128223840a464440459928
mynameisny <mynameisny@qq.com>
2021-09-03 22:45:42 +0800

    project init.

---
+++ server/src/test/java/me/ningyu/app/shuttleserver/ServerApplicationTests.java
@@ -0,0 +1,15 @@
+package me.ningyu.app.shuttleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
