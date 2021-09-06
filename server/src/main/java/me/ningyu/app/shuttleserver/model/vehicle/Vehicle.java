6b49a182ee1084c040a5626f18260843d6214c0e
ningyu <mynameisny@qq.com>
2021-09-06 10:38:34 +0800

    [feat]为载具类添加座位数和驾驶员属性

--- server/src/main/java/me/ningyu/app/shuttleserver/model/vehicle/Vehicle.java
+++ server/src/main/java/me/ningyu/app/shuttleserver/model/vehicle/Vehicle.java
@@ -1,6 +1,7 @@
 package me.ningyu.app.shuttleserver.model.vehicle;
 
 import lombok.Data;
+import me.ningyu.app.shuttleserver.model.people.Driver;
 
 /**
  * 载具
@@ -9,11 +10,24 @@ import lombok.Data;
 public abstract class Vehicle
 {
     private String id;
-
+    
+    /**
+     * 车型
+     */
     private Model model;
 
     /**
      * 车牌号
      */
     private String licensePlateNumber;
+    
+    /**
+     * 座位数（不包括司机）
+     */
+    private int seats;
+    
+    /**
+     * 这辆车当前的驾驶员
+     */
+    private Driver driver;
 }
