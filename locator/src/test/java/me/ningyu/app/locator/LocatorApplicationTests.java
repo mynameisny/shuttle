78418a2e9960ad9d7cac3623e34d2cd4af07f413
mynameisny <mynameisny@qq.com>
2021-09-24 22:53:54 +0800

    project init.

---
+++ locator/src/test/java/me/ningyu/app/locator/LocatorApplicationTests.java
@@ -0,0 +1,15 @@
+package me.ningyu.app.locator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocatorApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
