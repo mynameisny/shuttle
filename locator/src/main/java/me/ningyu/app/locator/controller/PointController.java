1512e0821df769db3cd2bca4dca99470a0cca0c4
mynameisny <mynameisny@qq.com>
2021-11-07 23:11:58 +0800

    [feat]添加列出坐标点的方法

--- locator/src/main/java/me/ningyu/app/locator/controller/PointController.java
+++ locator/src/main/java/me/ningyu/app/locator/controller/PointController.java
@@ -2,18 +2,25 @@ package me.ningyu.app.locator.controller;
 
 import me.ningyu.app.locator.vo.Point;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/points")
 public class PointController
 {
     @PostMapping
-    public ResponseEntity add(@RequestBody Point point)
+    public ResponseEntity<?> add(@RequestBody Point point)
     {
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<?> list()
+    {
+        List<Object> result = new ArrayList<>();
+        return ResponseEntity.ok(result);
     }
 }
