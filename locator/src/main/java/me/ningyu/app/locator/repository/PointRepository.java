72040507dcf9d0ac622b3eed84d2823b1636e694
mynameisny <mynameisny@qq.com>
2021-11-18 23:16:36 +0800

    [feat]Repository继承QueryDslPredicateExecutor接口

--- locator/src/main/java/me/ningyu/app/locator/repository/PointRepository.java
+++ locator/src/main/java/me/ningyu/app/locator/repository/PointRepository.java
@@ -2,8 +2,9 @@ package me.ningyu.app.locator.repository;
 
 import me.ningyu.app.locator.entity.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddpointRepository extends JpaRepository<Point, Long>
+public interface PointRepository extends JpaRepository<Point, Long>, QuerydslPredicateExecutor<Point>
 {}
