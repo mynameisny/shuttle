45e133b3d73464bb672f0e1a4b1ece97d2de8180
mynameisny <mynameisny@qq.com>
2022-01-09 22:05:21 +0800

    [feature]添加站点管理接口

---
+++ locator/src/main/java/me/ningyu/app/locator/controller/StationController.java
@@ -0,0 +1,12 @@
+package me.ningyu.app.locator.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Api(tags = "站点管理接口")
+@RestController
+@Slf4j
+public class StationController
+{}
