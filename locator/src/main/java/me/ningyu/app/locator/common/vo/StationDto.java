574011abb3b1e0d6a3bd3189906f85f68ade5400
mynameisny <mynameisny@qq.com>
2022-02-12 22:16:09 +0800

    [feat]添加站点的Dto

--- locator/src/main/java/me/ningyu/app/locator/common/vo/StationDto.java
+++ locator/src/main/java/me/ningyu/app/locator/common/vo/StationDto.java
@@ -2,6 +2,35 @@ package me.ningyu.app.locator.common.vo;
 
 import lombok.Data;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+
 @Data
 public class StationDto
-{}
+{
+
+    /**
+     * 站点名称
+     */
+    private String name;
+
+    /**
+     * 站点地址
+     */
+    private String address;
+
+    /**
+     * 站点描述
+     */
+    private String description;
+
+    /**
+     * 纬度
+     */
+    private Long latitude;
+
+    /**
+     * 经度
+     */
+    private Long longitude;
+}
