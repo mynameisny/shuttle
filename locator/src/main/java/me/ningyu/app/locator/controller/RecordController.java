7d30ae3d57c03d1437deb069c9a3af9713bdcbb3
mynameisny <mynameisny@qq.com>
2022-02-27 23:36:03 +0800

    [feature]支持审计操作

---
+++ locator/src/main/java/me/ningyu/app/locator/controller/RecordController.java
@@ -0,0 +1,13 @@
+package me.ningyu.app.locator.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "记录管理接口")
+@RestController
+@RequestMapping("/records")
+@Slf4j
+public class RecordController
+{}
