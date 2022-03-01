bc85adb14c335a624a1c676b53140e4331d4713f
mynameisny <mynameisny@qq.com>
2022-03-01 22:53:59 +0800

    [feature]支持删除记录

--- locator/src/main/java/me/ningyu/app/locator/controller/RecordController.java
+++ locator/src/main/java/me/ningyu/app/locator/controller/RecordController.java
@@ -9,10 +9,7 @@ import me.ningyu.app.locator.domain.record.entity.Record;
 import me.ningyu.app.locator.service.RecordService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -36,5 +33,12 @@ public class RecordController
         Record saved = recordService.add(dto);
         URI location = builder.replacePath("/records/{id}").buildAndExpand(saved.getId()).toUri();
         return ResponseEntity.created(location).body(saved);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remove(@PathVariable String id)
+    {
+        recordService.remove(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- locator/src/main/java/me/ningyu/app/locator/service/RecordService.java
+++ locator/src/main/java/me/ningyu/app/locator/service/RecordService.java
@@ -1,12 +1,15 @@
 package me.ningyu.app.locator.service;
 
 import lombok.extern.slf4j.Slf4j;
+import me.ningyu.app.locator.common.exception.NotfoundException;
 import me.ningyu.app.locator.common.vo.RecordDto;
-import me.ningyu.app.locator.domain.map.entity.Station;
 import me.ningyu.app.locator.domain.record.entity.Record;
 import me.ningyu.app.locator.domain.record.repository.RecordRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -25,5 +28,12 @@ public class RecordService
         Record entity = new Record();
         BeanUtils.copyProperties(dto, entity);
         return recordRepository.save(entity);
+    }
+
+    @Transactional
+    public void remove(String id)
+    {
+        Optional.of(recordRepository.findById(id)).get().orElseThrow(() -> new NotfoundException(String.format("记录%s不存在", id)));
+        recordRepository.deleteById(id);
     }
 }
