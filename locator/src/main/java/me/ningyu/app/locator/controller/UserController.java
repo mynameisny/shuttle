f02778336cb8e3ee80bb736db44bb95803a34c76
mynameisny <mynameisny@qq.com>
2022-03-05 22:47:33 +0800

    [feature]初始化用户管理Controller

---
+++ locator/src/main/java/me/ningyu/app/locator/controller/UserController.java
@@ -0,0 +1,14 @@
+package me.ningyu.app.locator.controller;
+
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "用户管理接口")
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController
+{}
