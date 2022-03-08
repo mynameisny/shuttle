68349473122a058c82d4a9258e55ec8da076e2a8
mynameisny <mynameisny@qq.com>
2022-03-08 22:48:11 +0800

    [feature]支持删除用户

--- locator/src/main/java/me/ningyu/app/locator/controller/UserController.java
+++ locator/src/main/java/me/ningyu/app/locator/controller/UserController.java
@@ -10,10 +10,7 @@ import me.ningyu.app.locator.domain.user.entity.User;
 import me.ningyu.app.locator.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -39,5 +36,12 @@ public class UserController
         User saved = userService.add(dto);
         URI location = builder.replacePath("/users/{id}").buildAndExpand(saved.getId()).toUri();
         return ResponseEntity.created(location).body(saved);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remove(@PathVariable String id)
+    {
+        userService.remove(id);
+        return ResponseEntity.noContent().build();
     }
 }
