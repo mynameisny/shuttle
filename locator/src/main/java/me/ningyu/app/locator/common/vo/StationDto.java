2aaced34e20273f9ad3342bbd8a5f008dd65c9ed
mynameisny <mynameisny@qq.com>
2022-03-26 23:09:57 +0800

    [fix]修正没有无参构造方法的问题

--- locator/src/main/java/me/ningyu/app/locator/common/vo/StationDto.java
+++ locator/src/main/java/me/ningyu/app/locator/common/vo/StationDto.java
@@ -1,13 +1,16 @@
 package me.ningyu.app.locator.common.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StationDto
 {
-
     /**
      * 站点名称
      */
--- locator/src/main/java/me/ningyu/app/locator/common/vo/UserDto.java
+++ locator/src/main/java/me/ningyu/app/locator/common/vo/UserDto.java
@@ -1,9 +1,13 @@
 package me.ningyu.app.locator.common.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserDto
 {
