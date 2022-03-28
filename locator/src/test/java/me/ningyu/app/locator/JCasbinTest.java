cfa16fa4d876c553acbb2fb447980bc2e945dd54
mynameisny <mynameisny@qq.com>
2022-03-28 23:25:05 +0800

    [feat]添加jcasbin adaptor

---
+++ locator/src/test/java/me/ningyu/app/locator/JCasbinTest.java
@@ -0,0 +1,56 @@
+package me.ningyu.app.locator;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.casbin.adapter.JDBCAdapter;
+import org.casbin.jcasbin.main.Enforcer;
+import org.casbin.jcasbin.model.Model;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JCasbinTest
+{
+    @Test
+    public void testJdbcAdapter() throws Exception
+    {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/shuttle_locator";
+        String username = "root";
+        String password = "123456";
+
+        // The adapter will use the table named "casbin_rule".
+        // Use driver, url, username and password to initialize adapter JDBC adapter.
+        JDBCAdapter adapter = new JDBCAdapter(driver, url, username, password);
+
+        // Recommend use DataSource to initialize adapter JDBC adapter.
+        // Implementer of DataSource interface, such as hikari, c3p0, durid, etc.
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+
+        adapter = new JDBCAdapter(dataSource);
+
+        Model model = new Model();
+        File file = new File("src/main/resources/jcasbin/rbac_model.conf");
+        String canonicalPath = file.getCanonicalPath();
+        model.loadModel(canonicalPath);
+
+        Enforcer e = new Enforcer(model, adapter);
+
+        // Check the permission.
+        e.enforce("alice", "data1", "read");
+
+        // Modify the policy.
+        // e.addPolicy(...);
+        // e.removePolicy(...);
+
+        // Save the policy back to DB.
+        e.savePolicy();
+        // Close the connection.
+        adapter.close();
+    }
+}
