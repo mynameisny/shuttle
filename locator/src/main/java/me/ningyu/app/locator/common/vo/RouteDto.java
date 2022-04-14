4a0535c30ed2b2ddeb12355c48a0daaf7ff026b3
mynameisny <mynameisny@qq.com>
2022-04-14 22:18:36 +0800

    [feat]添加线路Dto

---
+++ locator/src/main/java/me/ningyu/app/locator/common/vo/RouteDto.java
@@ -0,0 +1,28 @@
+package me.ningyu.app.locator.common.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RouteDto
+{
+    /**
+     * 线路名称
+     */
+    private String name;
+
+    /**
+     * 线路编号
+     */
+    private String no;
+
+    /**
+     * 线路描述
+     */
+    private String description;
+}
