60ffceeccb83ca3884bc1f82e100ebfd00e4ba4b
mynameisny <mynameisny@qq.com>
2022-05-07 23:12:37 +0800

    [feat]支持根据线路的起点和终点来过滤列表

--- locator/src/main/java/me/ningyu/app/locator/controller/RouteController.java
+++ locator/src/main/java/me/ningyu/app/locator/controller/RouteController.java
@@ -79,6 +79,8 @@ public class RouteController
             bindings.bind(root.name).first(StringExpression::containsIgnoreCase);
             bindings.bind(root.code).first(StringExpression::eq);
             bindings.bind(root.colorHex).first(StringExpression::eq);
+            bindings.bind(root.origin).first(StringExpression::containsIgnoreCase);
+            bindings.bind(root.terminal).first(StringExpression::containsIgnoreCase);
         }
     }
 }
