82d084a1a273f4b4235dae2c9c74145eeb8953d0
mynameisny <mynameisny@qq.com>
2022-10-26 23:45:29 +0800

    [feat] 添加全局异常处理，通过Slf4j打印异常

--- locator/src/main/java/me/ningyu/app/locator/common/exception/GlobalExceptionHandler.java
+++ locator/src/main/java/me/ningyu/app/locator/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package me.ningyu.app.locator.common.exception;
 
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler
 {
     @ExceptionHandler(NotfoundException.class)
@@ -16,6 +19,8 @@ public class GlobalExceptionHandler
     {
         response.setStatus(HttpStatus.NOT_FOUND.value());
         NotfoundException exception = (NotfoundException) e;
+        log.info("程序异常：{}", exception.getMsg(), exception);
+
         return ResponseEntity.notFound().build();
     }
 }
