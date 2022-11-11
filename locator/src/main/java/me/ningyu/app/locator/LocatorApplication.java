4febf85b036e79096ba1473389e2784735786785
mynameisny <mynameisny@qq.com>
2022-11-12 01:03:16 +0800

    [feature] 添加数据库审计

--- locator/src/main/java/me/ningyu/app/locator/LocatorApplication.java
+++ locator/src/main/java/me/ningyu/app/locator/LocatorApplication.java
@@ -2,8 +2,10 @@ package me.ningyu.app.locator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class LocatorApplication
 {
 
