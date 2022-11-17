92931a7572f38d8732b17e31df21206e519498f0
mynameisny <mynameisny@qq.com>
2022-11-17 23:31:18 +0800

    [fix] 支持根据地址的描述信息进行查询

--- locator/src/main/java/me/ningyu/app/locator/domain/map/entity/Address.java
+++ locator/src/main/java/me/ningyu/app/locator/domain/map/entity/Address.java
@@ -30,10 +30,10 @@ public class Address extends Variable
     @Column(name = "latitude", columnDefinition = "VARCHAR(50) NOT NULL DEFAULT '' COMMENT '纬度'")
     private String latitude;
 
-    @Column(name = "longitude", columnDefinition = "VARCHAR(50) NOT NULL DEFAULT '' COMMENT '经度'")
+    @Column(name = "longitude", columnDefinition = "DOUBLE(50) NOT NULL DEFAULT 0.0 COMMENT '经度'")
     private String longitude;
 
-    @Column(name = "area_code", columnDefinition = "VARCHAR(50) NOT NULL DEFAULT '' COMMENT '地区编码，例如220503100001表示鸭园社区'")
+    @Column(name = "area_code", columnDefinition = "DOUBLE(50) NOT NULL DEFAULT 0.0 COMMENT '地区编码，例如220503100001表示鸭园社区'")
     private String areaCode;
 
     @ElementCollection(fetch = FetchType.LAZY)
