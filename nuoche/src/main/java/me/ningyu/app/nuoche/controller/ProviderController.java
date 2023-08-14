d6e5a5f3a415a47a663d8c8e173c8933e654311a
mynameisny <mynameisny@qq.com>
2023-08-14 23:44:42 +0800

    Feat: 通知服务商实体定义

--- nuoche/src/main/java/me/ningyu/app/nuoche/controller/ProviderController.java
+++ nuoche/src/main/java/me/ningyu/app/nuoche/controller/ProviderController.java
@@ -1,20 +1,14 @@
 package me.ningyu.app.nuoche.controller;
 
-import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.querydsl.core.types.Predicate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.ningyu.app.nuoche.common.dto.ProviderDTO;
-import me.ningyu.app.nuoche.common.dto.UserDTO;
 import me.ningyu.app.nuoche.common.dto.validation.ProviderAdd;
-import me.ningyu.app.nuoche.common.dto.validation.UserAdd;
 import me.ningyu.app.nuoche.common.vo.ProviderVO;
-import me.ningyu.app.nuoche.common.vo.UserVO;
 import me.ningyu.app.nuoche.controller.binder.UserSearchBinding;
-import me.ningyu.app.nuoche.domain.Provider;
 import me.ningyu.app.nuoche.domain.User;
 import me.ningyu.app.nuoche.service.ProviderService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
@@ -23,7 +17,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.transaction.Transactional;
 import javax.validation.groups.Default;
 import java.net.URI;
 
--- nuoche/src/main/java/me/ningyu/app/nuoche/domain/Provider.java
+++ nuoche/src/main/java/me/ningyu/app/nuoche/domain/Provider.java
@@ -18,20 +18,20 @@ import java.util.Objects;
 @ToString
 public class Provider extends Variable
 {
-    @Column(name = "vendor", columnDefinition = "VARCHAR(50) DEFAULT 0 COMMENT '推送服务的服务商'")
+    @Column(name = "vendor", columnDefinition = "VARCHAR(50) DEFAULT 0 COMMENT '推送服务的服务商：NTFY或BARK等'")
     @Enumerated(value = EnumType.STRING)
     private ProviderVendor vendor;
 
     @Column(name = "url", columnDefinition = "VARCHAR(150) DEFAULT 0 COMMENT '推送到设备的URL地址'")
     private String url;
 
-    @Column(name = "push_key", columnDefinition = "VARCHAR(50) DEFAULT 0 COMMENT '推送网站提供的认证Key'")
+    @Column(name = "push_key", columnDefinition = "VARCHAR(100) DEFAULT 0 COMMENT '推送网站提供的认证Key'")
     private String pushKey;
 
     @Column(name = "description", columnDefinition = "TEXT DEFAULT '' COMMENT '描述'")
     private String description;
 
-    @Column(name = "weight", columnDefinition = "TINYINT(1) DEFAULT 0 COMMENT '权重'")
+    @Column(name = "weight", columnDefinition = "INT(11) DEFAULT 0 COMMENT '权重'")
     private int weight = 0;
 
     @Column(name = "enabled", columnDefinition = "TINYINT(1) DEFAULT 1 COMMENT '是否启用'")
