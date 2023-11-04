5d7e1b31e2ceffa90458fd303afe7615cf9a0681
mynameisny <mynameisny@qq.com>
2023-11-04 23:30:24 +0800

    Feat: 定义莸得QRCode的方法（Base64）

--- nuoche/src/main/java/me/ningyu/app/nuoche/service/QRCodeService.java
+++ nuoche/src/main/java/me/ningyu/app/nuoche/service/QRCodeService.java
@@ -1,16 +1,21 @@
 package me.ningyu.app.nuoche.service;
 
 import lombok.RequiredArgsConstructor;
+import me.ningyu.app.nuoche.common.QRCodeUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.OutputStream;
 
 @Service
 @RequiredArgsConstructor
 public class QRCodeService
 {
-    public String generateBase64String()
+    public OutputStream generateBase64String(String content) throws Exception
     {
+
+        QRCodeUtils.encode(content, null);
         return null;
     }
 }
