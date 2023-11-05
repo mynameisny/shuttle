0bae637a467b69441f5eff06b686fed87cda7ff6
mynameisny <mynameisny@qq.com>
2023-11-05 23:40:20 +0800

    Feat: 创建BufferedImage

--- nuoche/src/main/java/me/ningyu/app/nuoche/service/QRCodeService.java
+++ nuoche/src/main/java/me/ningyu/app/nuoche/service/QRCodeService.java
@@ -1,12 +1,16 @@
 package me.ningyu.app.nuoche.service;
 
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.common.BitMatrix;
 import lombok.RequiredArgsConstructor;
 import me.ningyu.app.nuoche.common.QRCodeUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.awt.image.BufferedImage;
 import java.io.OutputStream;
+import java.util.Hashtable;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +21,28 @@ public class QRCodeService
 
         QRCodeUtils.encode(content, null);
         return null;
+    }
+
+    public static BufferedImage createImage(String content) throws Exception {
+        // 二维码参数设置
+        Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+        // 安全等级，最高h
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        // 编码设置
+        hints.put(EncodeHintType.CHARACTER_SET, CHARSET);
+        // 设置margin=0-10
+        hints.put(EncodeHintType.MARGIN, 1);
+        // 创建矩阵容器
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, QRCODE_SIZE, QRCODE_SIZE, hints);
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        // 矩阵转换图像
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+            }
+        }
+        return image;
     }
 }
