3bc3d68afe4276dc97afbdfd4316b0cdb22fde26
mynameisny <mynameisny@qq.com>
2023-11-13 23:52:48 +0800

    Feat: QRCode支持在中间插入图片

--- nuoche/src/main/java/me/ningyu/app/nuoche/service/QRCodeService.java
+++ nuoche/src/main/java/me/ningyu/app/nuoche/service/QRCodeService.java
@@ -1,22 +1,15 @@
 package me.ningyu.app.nuoche.service;
 
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.common.BitMatrix;
 import lombok.RequiredArgsConstructor;
 import me.ningyu.app.nuoche.common.QRCodeUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
-import java.util.Hashtable;
 
 @Service
 @RequiredArgsConstructor
@@ -29,37 +22,6 @@ public class QRCodeService
         return null;
     }
 
-    public static void createQR(String data, String path, String charset, Map hashMap, int height, int width) throws WriterException, IOException
-    {
-
-        BitMatrix matrix = new MultiFormatWriter().encode(
-                new String(data.getBytes(charset), charset),
-                BarcodeFormat.QR_CODE, width, height);
-
-        MatrixToImageWriter.writeToFile(
-                matrix,
-                path.substring(path.lastIndexOf('.') + 1),
-                new File(path));
-    }
-
-    /**
-     * 生成二维码
-     *
-     * @param content 内容
-     * @param output  输出流
-     * @throws Exception
-     */
-    public static void encode(String content, OutputStream output) throws Exception
-    {
-        content = StringUtils.normalizeSpace(content);
-        BufferedImage image = createImage(content);
-        ImageIO.write(image, "JPG", output);
-
-        BufferedImage dstImage = image.getSubimage(228, 705, 359, 287);
-
-        File outFile = new File("/Users/xxx/Downloads/output.png");
-        ImageIO.write(dstImage, "png", outFile);
-    }
 
     public static ByteBuffer getSubImage(ByteBuffer imageContent, int x, int y, int width, int height) throws    Exception {
         ByteArrayInputStream in = new ByteArrayInputStream(imageContent.array());
