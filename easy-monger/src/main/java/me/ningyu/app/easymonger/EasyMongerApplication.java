a6b8c576e71dcbf726f7e0998930d4b4e275fc0b
mynameisny <mynameisny@qq.com>
2024-09-29 22:52:42 +0800

    Feat: 新增Module，使用JDK-21，取代JDK-11

---
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/EasyMongerApplication.java
@@ -0,0 +1,15 @@
+package me.ningyu.app.easymonger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMongerApplication
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EasyMongerApplication.class, args);
+    }
+    
+}
---
+++ easy-monger/src/test/java/me/ningyu/app/easymonger/EasyMongerApplicationTests.java
@@ -0,0 +1,15 @@
+package me.ningyu.app.easymonger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMongerApplicationTests
+{
+    
+    @Test
+    void contextLoads()
+    {
+    }
+    
+}
