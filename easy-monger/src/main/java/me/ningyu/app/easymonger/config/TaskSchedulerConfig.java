9a6d17536627048a97ef37f7cd4008f350f582ce
mynameisny <mynameisny@qq.com>
2024-10-02 23:40:30 +0800

    Feat: 添加定时任务的配置

---
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/config/TaskSchedulerConfig.java
@@ -0,0 +1,21 @@
+package me.ningyu.app.easymonger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class TaskSchedulerConfig
+{
+    @Bean
+    public TaskScheduler taskScheduler()
+    {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.initialize();
+        return scheduler;
+    }
+}
