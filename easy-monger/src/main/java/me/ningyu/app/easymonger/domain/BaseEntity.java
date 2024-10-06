d0f5bae9f9fbf56941ce2122dbe55b2f99207412
mynameisny <mynameisny@qq.com>
2024-10-06 23:25:33 +0800

    Feat: 重新定义基础实体的UUID生成策略
    GenericGenerator注解从Hibernate 6.5版本之后已经过时

--- easy-monger/src/main/java/me/ningyu/app/easymonger/domain/BaseEntity.java
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/domain/BaseEntity.java
@@ -1,8 +1,10 @@
 package me.ningyu.app.easymonger.domain;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -11,19 +13,22 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * 使用 hibernate UUIDHexGenerator 会生成一条警告，说生成的 uuid 不符合规范，推荐使用 UUIDGenerator，生成的字符串默认有 '-' 连接，导致最大长度长于 32，这里把 @column 去掉，使用默认 255 长度
  */
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class BaseEntity implements Serializable
 {
     @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(columnDefinition = " CHAR(36) NOT NULL COMMENT '主键id'  ", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "CHAR(36) NOT NULL COMMENT '主键id'", nullable = false)
     private String id;
 
     @Version
@@ -47,4 +52,27 @@ public class BaseEntity implements Serializable
 
     @Column(name = "is_deleted", columnDefinition = " tinyint(1) default 0", nullable = false)
     private int isDeleted;
+    
+    @Override
+    public final boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity)) return false;
+        
+        BaseEntity that = (BaseEntity) o;
+        return isDeleted == that.isDeleted && id.equals(that.id) && Objects.equals(version, that.version) && Objects.equals(createdDate, that.createdDate) && Objects.equals(modifiedDate, that.modifiedDate) && Objects.equals(createdBy, that.createdBy) && Objects.equals(lastModifiedBy, that.lastModifiedBy);
+    }
+    
+    @Override
+    public int hashCode()
+    {
+        int result = id.hashCode();
+        result = 31 * result + Objects.hashCode(version);
+        result = 31 * result + Objects.hashCode(createdDate);
+        result = 31 * result + Objects.hashCode(modifiedDate);
+        result = 31 * result + Objects.hashCode(createdBy);
+        result = 31 * result + Objects.hashCode(lastModifiedBy);
+        result = 31 * result + isDeleted;
+        return result;
+    }
 }
