a4fbe6c4f0914549c0834a38ed88ec29222be622
mynameisny <mynameisny@qq.com>
2024-10-14 17:16:14 +0800

    Feat: 抽取出BaseVo

--- easy-monger/src/main/java/me/ningyu/app/easymonger/model/vo/BaseVo.java
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/model/vo/BaseVo.java
@@ -10,35 +10,18 @@ import me.ningyu.app.easymonger.model.enums.Gender;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-/**
- * VO for {@link me.ningyu.app.easymonger.domain.auth.User}
- */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class UserVo extends BaseVo
+public class BaseVo implements Serializable
 {
     private String id;
-    
-    private String code;
-    
-    private String name;
-    
-    private Gender gender;
-    
-    private String mobile;
-    
-    private String email;
-    
+    private String tenant;
     private String remark;
-    
     private LocalDateTime createdDate;
-    
     private LocalDateTime modifiedDate;
-    
     private String createdBy;
-    
     private String lastModifiedBy;
 }
