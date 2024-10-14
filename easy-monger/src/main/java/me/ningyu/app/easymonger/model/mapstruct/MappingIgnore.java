26a31bc71eaaefb1c3a3ade3b94216a77b6c671e
mynameisny <mynameisny@qq.com>
2024-10-14 17:09:52 +0800

    Feat: 编写mapstruct的自定义注解，定义需要忽略公共字段
    在controller接口的方法上使用@MappingIgnore注解

---
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/model/mapstruct/MappingIgnore.java
@@ -0,0 +1,26 @@
+package me.ningyu.app.easymonger.model.mapstruct;
+
+
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 映射时忽略公共字段，在controller接口的方法上使用@MappingIgnore注解
+ * 参考：
+ * - https://mapstruct.org/documentation/stable/reference/html/#mapping-composition
+ * - https://blog.csdn.net/zyw61483/article/details/112990760
+ */
+@Retention(RetentionPolicy.CLASS)
+@Mappings(value = {
+        @Mapping(target = "id", ignore = true),
+        @Mapping(target = "createdDate", ignore = true),
+        @Mapping(target = "modifiedDate", ignore = true),
+        @Mapping(target = "createdBy", ignore = true),
+        @Mapping(target = "lastModifiedBy", ignore = true)
+})
+public @interface MappingIgnore
+{
+}
