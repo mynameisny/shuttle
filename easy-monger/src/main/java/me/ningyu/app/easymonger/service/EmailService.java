0b475a3e5f1681ed1928acf6ceb74afaec028961
mynameisny <mynameisny@qq.com>
2024-10-28 22:42:15 +0800

    Feat: 添加用于发送邮件激活码的方法

--- easy-monger/src/main/java/me/ningyu/app/easymonger/service/EmailService.java
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/service/EmailService.java
@@ -1,10 +1,14 @@
 package me.ningyu.app.easymonger.service;
 
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailService
 {
     private final JavaMailSender mailSender;
@@ -24,5 +28,25 @@ public class EmailService
         message.setFrom("MS_jA3xWF@trial-pr9084z7kvvgw63d.mlsender.net");
         
         mailSender.send(message);
+    }
+    
+    public void sendActivationEmail(String to, String activationLink)
+    {
+        try
+        {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject("激活您的账户");
+            helper.setText("点击以下链接激活您的账户：<a href=\"" + activationLink + "\">激活链接</a>", true);
+            
+            // 发送邮件
+            mailSender.send(message);
+            System.out.println("邮件发送成功");
+        }
+        catch (Exception exception)
+        {
+            log.error("给{}发送激活邮件时出现错误", to, exception);
+        }
     }
 }
