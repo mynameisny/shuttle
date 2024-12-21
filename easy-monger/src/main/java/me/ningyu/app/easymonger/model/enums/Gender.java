3c2a094e2c7f756a9ea699cad87aa4c11ff155c3
mynameisny <mynameisny@qq.com>
2024-12-21 22:48:33 +0800

    Feat: 根据汉字查找对应的枚举

--- easy-monger/src/main/java/me/ningyu/app/easymonger/model/enums/Gender.java
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/model/enums/Gender.java
@@ -6,16 +6,27 @@ import lombok.Getter;
 public enum Gender
 {
     MALE("男"),
-
+    
     FEMALE("女"),
-
+    
     OTHER("不方便透露");
-
+    
     private final String name;
-
+    
     Gender(String name)
     {
         this.name = name;
     }
     
+    public static Gender fromName(String name)
+    {
+        for (Gender gender : Gender.values())
+        {
+            if (gender.getName().equals(name))
+            {
+                return gender;
+            }
+        }
+        throw new IllegalArgumentException("Unknown gender name: " + name);
+    }
 }
