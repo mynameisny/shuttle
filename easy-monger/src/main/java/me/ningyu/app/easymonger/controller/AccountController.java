26c4653e4f3b344e2ebc59f7384663669c20c2ba
mynameisny <mynameisny@qq.com>
2024-12-29 22:12:31 +0800

    Feat: 添加账号controller

---
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/controller/AccountController.java
@@ -0,0 +1,154 @@
+package me.ningyu.app.easymonger.controller;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.EnumExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.ningyu.app.easymonger.model.AccountDto;
+import me.ningyu.app.easymonger.model.AccountVo;
+import me.ningyu.app.easymonger.model.ParseTemplateDto;
+import me.ningyu.app.easymonger.model.ParseTemplateVo;
+import me.ningyu.app.easymonger.service.AccountService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/accounts")
+@RequiredArgsConstructor
+@Slf4j
+public class AccountController
+{
+    private final AccountService accountService;
+
+
+    @PostMapping
+    public ResponseEntity<AccountVo> add(@Validated @RequestBody AccountDto dto)
+    {
+        AccountVo vo = accountService.add(dto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", UriComponentsBuilder.fromUriString("/accounts/{accountId}").buildAndExpand(vo.getId()).toUriString());
+
+        return new ResponseEntity<>(vo, headers, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id)
+    {
+        accountService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountVo> update(@PathVariable String id, @RequestBody AccountDto dto)
+    {
+        AccountVo vo = accountService.update(id, dto);
+        return ResponseEntity.ok(vo);
+    }
+
+    @GetMapping
+    public Page<AccountVo> list(@QuerydslPredicate(root = Account.class, bindings = AccountSearchBinding.class) Predicate predicate, @PageableDefault(size = 20) @SortDefault.SortDefaults({@SortDefault(sort = "modifiedDate", direction = Sort.Direction.DESC), @SortDefault(sort = "id", direction = Sort.Direction.ASC)}) Pageable pageable)
+    {
+        return accountService.list(predicate, pageable);
+    }
+
+    @GetMapping("/{id}")
+    public AccountVo get(@PathVariable String id)
+    {
+        return accountService.get(id);
+    }
+
+    @PostMapping("/{accountId}/parse-templates")
+    public ResponseEntity<ParseTemplateVo> addParseTemplate(@PathVariable String accountId, @Validated @RequestBody ParseTemplateDto dto)
+    {
+        ParseTemplateVo vo = accountService.addParseTemplate(accountId, dto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", UriComponentsBuilder.fromUriString("/accounts/{accountId}/parse-templates/{id}").buildAndExpand(accountId, vo.getId()).toUriString());
+
+        return new ResponseEntity<>(vo, headers, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{accountId}/parse-templates")
+    public ResponseEntity<Void> deleteParseTemplates(@PathVariable String accountId, @RequestBody(required = false) List<String> parseTemplateIds)
+    {
+        accountService.deleteParseTemplates(accountId, parseTemplateIds);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{accountId}/parse-templates")
+    public Page<ParseTemplateVo> listParseTemplates(@PathVariable String accountId, @QuerydslPredicate(root = ParseTemplate.class, bindings = ParseTemplateSearchBinding.class) Predicate predicate, @PageableDefault(size = 20) @SortDefault.SortDefaults({@SortDefault(sort = "modifiedDate", direction = Sort.Direction.DESC), @SortDefault(sort = "id", direction = Sort.Direction.ASC)}) Pageable pageable)
+    {
+        return accountService.listParseTemplates(accountId, predicate, pageable);
+    }
+
+    private static class AccountSearchBinding implements QuerydslBinderCustomizer<QAccount>
+    {
+        @Override
+        public void customize(QuerydslBindings bindings, QAccount root)
+        {
+            bindings.bind(QAccount.account.id).first(StringExpression::eq);
+            bindings.bind(root.id).first(StringExpression::eq);
+            bindings.bind(root.user.id).first(StringExpression::eq);
+            bindings.bind(root.platform).first(EnumExpression::eq);
+            bindings.bind(root.mobile).first(StringExpression::eq);
+            bindings.bind(root.email).first(StringExpression::eq);
+        }
+    }
+
+    private static class ParseTemplateSearchBinding implements QuerydslBinderCustomizer<QParseTemplate>
+    {
+        @Override
+        public void customize(QuerydslBindings bindings, QParseTemplate root)
+        {
+            bindings.bind(QParseTemplate.parseTemplate.id).first(StringExpression::eq);
+            bindings.bind(QParseTemplate.parseTemplate.account.id).first(StringExpression::eq);
+            bindings.bind(root.id).first(StringExpression::eq);
+            bindings.bind(root.name).all(this::nameIn);
+            bindings.bind(root.deviceName).all((path, value) ->
+            {
+                if (value.size() == 1)
+                {
+                    return Optional.of(path.eq(value.iterator().next()));
+                }
+                else
+                {
+                    return Optional.of(path.in(value));
+                }
+            });
+            bindings.bind(root.enabled).first(BooleanExpression::eq);
+            bindings.bind(root.remark).first(StringExpression::contains);
+        }
+
+        public Optional<Predicate> nameIn(StringPath path, Collection<? extends String> value)
+        {
+            if (value.size() == 1)
+            {
+                return Optional.of(path.eq(value.iterator().next()));
+            }
+            else
+            {
+                return Optional.of(path.in(value));
+            }
+        }
+    }
+}
---
+++ easy-monger/src/main/java/me/ningyu/app/easymonger/service/AccountService.java
@@ -0,0 +1,206 @@
+package me.ningyu.app.easymonger.service;
+
+import com.github.dozermapper.core.Mapper;
+import com.querydsl.core.types.Predicate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.ningyu.app.easymonger.domain.auth.User;
+import me.ningyu.app.easymonger.domain.auth.UserRepository;
+import me.ningyu.app.easymonger.domain.coupon.*;
+import me.ningyu.app.easymonger.exception.DuplicateException;
+import me.ningyu.app.easymonger.exception.NotFoundException;
+import me.ningyu.app.easymonger.model.enums.Field;
+import me.ningyu.app.easymonger.model.vo.AccountVo;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AccountService
+{
+    private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
+    private final BrandRepository brandRepository;
+    private final ParseTemplateRepository parseTemplateRepository;
+    private final ParseTemplateCoordinateRepository parseTemplateCoordinateRepository;
+    private final Mapper mapper;
+
+    @Transactional
+    public AccountVo add(AccountDto dto)
+    {
+        if (accountRepository.exists(QAccount.account.platform.eq(dto.getPlatform()).and(QAccount.account.mobile.eq(dto.getMobile()))))
+        {
+            throw new DuplicateException("账户已存在");
+        }
+
+        User user = userRepository.findById(dto.getUserId()).orElseThrow(() -> new NotFoundException("用户[{}]不存在", dto.getUserId()));
+
+        Account entity = dtoToEntity(dto);
+        entity.setUser(user);
+
+        accountRepository.save(entity);
+
+        return entityToVo(entity);
+    }
+
+    @Transactional
+    public void delete(String id)
+    {
+        if (!accountRepository.exists(QAccount.account.id.eq(id)))
+        {
+            throw new NotFoundException("账户不存在");
+        }
+
+        accountRepository.deleteById(id);
+    }
+
+    @Transactional
+    public AccountVo update(String id, AccountDto dto)
+    {
+        Account entity = accountRepository.findById(id).orElseThrow(() -> new NotFoundException("账户不存在"));
+
+        accountRepository.save(entity);
+
+        return entityToVo(entity);
+    }
+
+    public Page<AccountVo> list(Predicate predicate, Pageable pageable)
+    {
+        String userCode = SecurityContextHolder.getContext().getAuthentication().getName();
+        predicate = QAccount.account.user.code.eq(userCode).and(predicate);
+
+        Page<Account> users = accountRepository.findAll(predicate, pageable);
+
+        return users.map(this::entityToVo);
+    }
+
+    public AccountVo get(String id)
+    {
+        Account entity = accountRepository.findById(id).orElseThrow(() -> new NotFoundException("账户不存在"));
+        return entityToVo(entity);
+    }
+
+    private AccountVo entityToVo(Account entity)
+    {
+        AccountVo vo = new AccountVo();
+        mapper.map(entity, vo);
+        vo.setPlatformText(entity.getPlatform().getName());
+        return vo;
+    }
+
+    private Account dtoToEntity(AccountDto dto)
+    {
+        Account entity = new Account();
+        mapper.map(dto, entity);
+        return entity;
+    }
+
+    @Transactional
+    public ParseTemplateVo addParseTemplate(String accountId, ParseTemplateDto dto)
+    {
+        Account account = accountRepository.findById(accountId).orElseThrow(() -> new NotFoundException("找不到账号"));
+
+        checkParseTemplateParam(account, dto);
+
+        ParseTemplate parseTemplate = dtoToEntity(dto);
+        parseTemplate.setAccount(account);
+        parseTemplateRepository.save(parseTemplate);
+
+        for (Coordinate coordinate : dto.getCoordinates())
+        {
+            ParseTemplateCoordinate parseTemplateCoordinate = new ParseTemplateCoordinate();
+            parseTemplateCoordinate.setField(coordinate.getField());
+            parseTemplateCoordinate.setX(coordinate.getX());
+            parseTemplateCoordinate.setY(coordinate.getY());
+            parseTemplateCoordinate.setWidth(coordinate.getWidth());
+            parseTemplateCoordinate.setHeight(coordinate.getHeight());
+            parseTemplateCoordinate.setParseTemplate(parseTemplate);
+            parseTemplateCoordinateRepository.save(parseTemplateCoordinate);
+        }
+
+        account.getParseTemplates().add(parseTemplate);
+        accountRepository.save(account);
+
+        return entityToVo(parseTemplate);
+    }
+
+    @Transactional
+    public void deleteParseTemplates(String accountId, List<String> parseTemplateIds)
+    {
+        Account account = accountRepository.findById(accountId).orElseThrow(() -> new NotFoundException("找不到账号"));
+
+        Iterable<ParseTemplate> parseTemplates;
+        if (ObjectUtils.isEmpty(parseTemplateIds))
+        {
+            parseTemplates = parseTemplateRepository.findAll(QParseTemplate.parseTemplate.account.id.eq(account.getId()));
+        }
+        else
+        {
+            parseTemplates = parseTemplateRepository.findAll(QParseTemplate.parseTemplate.account.id.eq(account.getId()).and(QParseTemplate.parseTemplate.id.in(parseTemplateIds)));
+        }
+
+        parseTemplateRepository.deleteAll(parseTemplates);
+
+        accountRepository.save(account);
+    }
+
+    public Page<ParseTemplateVo> listParseTemplates(String accountId, Predicate predicate, Pageable pageable)
+    {
+        Account account = accountRepository.findById(accountId).orElseThrow(() -> new NotFoundException("找不到账号"));
+
+        predicate = QParseTemplate.parseTemplate.account.id.eq(account.getId()).and(predicate);
+
+        Page<ParseTemplate> parseTemplates = parseTemplateRepository.findAll(predicate, pageable);
+
+        return parseTemplates.map(this::entityToVo);
+    }
+
+    private ParseTemplate dtoToEntity(ParseTemplateDto dto)
+    {
+        ParseTemplate entity = new ParseTemplate();
+        mapper.map(dto, entity);
+        return entity;
+    }
+
+    private ParseTemplateVo entityToVo(ParseTemplate entity)
+    {
+        ParseTemplateVo vo = new ParseTemplateVo();
+        mapper.map(entity, vo);
+        vo.setAccountId(entity.getAccount().getId());
+        return vo;
+    }
+
+    private void checkParseTemplateParam(Account account, ParseTemplateDto dto)
+    {
+        if (dto.isEnabled())
+        {
+            account.getParseTemplates().stream().filter(ParseTemplate::isEnabled).findFirst().ifPresent(parseTemplate -> {
+                throw new DuplicateException(String.format("只能有一个被启用的解析模板，当前被启用的是：%s(%s)", parseTemplate.getName(), parseTemplate.getId()));
+            });
+        }
+
+        if (parseTemplateCoordinateRepository.exists(QParseTemplate.parseTemplate.account.id.eq(account.getId()).and(QParseTemplate.parseTemplate.name.eq(dto.getName()))))
+        {
+            throw new DuplicateException("解析模板名称已存在");
+        }
+
+        List<Field> fields = dto.getCoordinates().stream().map(Coordinate::getField).collect(Collectors.toList());
+        if (fields.stream().distinct().count() != fields.size())
+        {
+            throw new DuplicateException("相同的Field只能存在一个");
+        }
+
+        if (dto.getCoordinates().stream().distinct().count() != dto.getCoordinates().size())
+        {
+            throw new DuplicateException("相同的坐标只能存在一个");
+        }
+    }
+}
