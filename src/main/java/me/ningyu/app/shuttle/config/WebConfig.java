cc6c763b6fe57f22f1753fda0a15e400ec3619e7
mynameisny <mynameisny@qq.com>
2026-01-22 00:13:52 +0800

    Feat: 使用Spring4的版本控制功能

--- src/main/java/me/ningyu/app/shuttle/config/WebConfig.java
+++ src/main/java/me/ningyu/app/shuttle/config/WebConfig.java
@@ -7,19 +7,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer
 {
-    private final ApiVersionInterceptor apiVersionInterceptor;
 
-
-    public WebConfig(ApiVersionInterceptor apiVersionInterceptor)
-    {
-        this.apiVersionInterceptor = apiVersionInterceptor;
-    }
-
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry)
-    {
-        // 仅拦截 API 路径
-        registry.addInterceptor(apiVersionInterceptor).addPathPatterns("/api/**");
-    }
 }
